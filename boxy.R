440a921d1dd671b9a80efb1091fc7f3f44bae6cb
k8y1b <56333166+k8y1b@users.noreply.github.com>
2021-02-17 15:16:41 -0800

    Update boxy.R

--- boxy.R
+++ boxy.R
@@ -3,17 +3,25 @@ test <- read.csv("test.csv")
 
 varn = names(train)
 
-varnxs = varn[c(1:15, 56)]
+varnxs = varn[c(2:15, 56)]
+
+covertypes = c("spruce/fir", "lodgepole pine", "ponderosa pine", "cottonwood/willow", "aspen", "douglas fir", "krummholz")
 
 
-one <- subset(train, Cover_Type == 1, select = c(1:15, 56))
-two <- subset(train, Cover_Type == 2, select = c(1:15, 56))
+# deselect the soil type binary variables and create data frame for each cover type
 
-boxplot(one$Elevation, two$Elevation, xlab = "cover type", ylab = "elevation", names = c("one", "two"))
+one <- subset(train, Cover_Type == 1, select = c(1:11, 56))
+two <- subset(train, Cover_Type == 2, select = c(1:11, 56))
+three <- subset(train, Cover_Type == 3, select = c(1:11, 56))
+four <- subset(train, Cover_Type == 4, select = c(1:11, 56))
+five <- subset(train, Cover_Type == 5, select = c(1:11, 56))
+six <- subset(train, Cover_Type == 6, select = c(1:11, 56))
+seven <- subset(train, Cover_Type == 7, select = c(1:11, 56))
 
 
 
 
-for(i in 1:16) {
-    boxplot(one[,i], two[,i], xlab = "Cover_Type", ylab = varnxs[i], names = c("one", "two"))
+
+for(i in 1:4) {
+    boxplot(one[,i], two[,i], three[,i], four[,i], five[,i], six[,i], seven[,i], xlab = "Cover_Type", ylab = varnxs[i], names = covertypes)
 }
